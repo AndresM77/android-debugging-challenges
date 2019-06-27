7dcae07fa49e8bf8c73f1b9e17868d47d425fe07
Andres Montoya <andresmontoya7@fb.com>
2019-06-27 13:46:11 -0700

    Milestone 4

--- app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
@@ -2,6 +2,7 @@ package com.codepath.debuggingchallenges.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
@@ -23,6 +24,10 @@ public class MoviesActivity extends AppCompatActivity {
 
     private static final String API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
+    public final static String API_KEY_PARAM = "api_key";
+
+    public final static String API_BASE_URL = "https://api.themoviedb.org/3/movie/now_playing";
+
     RecyclerView rvMovies;
     MoviesAdapter adapter;
     ArrayList<Movie> movies;
@@ -32,12 +37,16 @@ public class MoviesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
         rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>();
 
         // Create the adapter to convert the array to views
-        MoviesAdapter adapter = new MoviesAdapter(movies);
+        adapter = new MoviesAdapter(movies);
 
         // Attach the adapter to a ListView
         rvMovies.setAdapter(adapter);
+
+        // Attach a layout manager
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
 
         fetchMovies();
     }
@@ -45,17 +54,27 @@ public class MoviesActivity extends AppCompatActivity {
 
     private void fetchMovies() {
         Log.d("MYAPP", "I got here");
-        String url = " https://api.themoviedb.org/3/movie/now_playing?api_key=";
+        String url = " https://api.themoviedb.org/3/movie/now_playing?api_key=" + API_KEY;
+        //url = url + API_KEY;
         AsyncHttpClient client = new AsyncHttpClient();
+        // setting the request parameters
+        //RequestParams params = new RequestParams();
+        //params.put(API_KEY_PARAM, API_KEY);
         client.get(url, null, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 try {
                     JSONArray moviesJson = response.getJSONArray("results");
-                    movies = Movie.fromJSONArray(moviesJson);
+                    movies.addAll(Movie.fromJSONArray(moviesJson));
+                    adapter.notifyDataSetChanged();
+                    Log.d("MYAPP", "I am getting movies");
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.e("MYAPP", "Failed configuration");
             }
         });
     }
